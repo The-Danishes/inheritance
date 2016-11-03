c3fcf4802356d967978a72fb5f40b9356dd43feb
Danish Mughal <mughal.danish@gmail.com>
2016-11-02 18:50:23 -0700

    Added modules example for car/bike code

---
+++ car_bike_modules.rb
@@ -0,0 +1,48 @@
+module Actions
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
+
+
+class Vehicle
+  def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+end
+
+class Car < Vehicle
+  include Actions
+
+  def initialize 
+    super
+    @gas_level = 10
+  end
+
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike < Vehicle
+  include Actions
+
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+car = Car.new
+car.honk_horn
+
+bike = Bike.new
+bike.ring_bell
